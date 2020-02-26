deda2a2d1d37daee5edfdaf235ed8c76bab7d67d
adramarilla <adriana.amarilla@unahur.edu.ar>
2020-02-26 00:04:16 -0300

    inicial

--- main.py
+++ main.py
@@ -5,11 +5,7 @@ logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message
 
 carpeta_imagenes = './imagenes'
 query = 'computadoras'
-<<<<<<< HEAD
-api = PixabayAPI('PONER ACÁ EL API KEY', carpeta_imagenes)
-=======
 api = PixabayAPI('15392204-778b8867cb956ec976dc3c8db', carpeta_imagenes)
->>>>>>> Initial commit
 
 logging.info(f'Buscando imagenes de {query}')
 urls = api.buscar_imagenes(query, 5)
